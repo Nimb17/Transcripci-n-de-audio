c24f09040d64442ab2df6c40f8f6c313ee886307
Hector-M <hola@nicobm.com>
2023-12-27 15:06:58 -0300

    Refactor loading state variables in HomePage component

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -14,7 +14,8 @@ const HomePage = () => {
   const [file, setFile] = useState(null);
   const [resultado, setResultado] = useState(null);
   const [resultado2, setResultado2] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [loadingWhisper, setLoadingWhisper] = useState(false);
+  const [loadingGpt, setLoadingGpt] = useState(false);
   const [modalIsOpen, setModalIsOpen] = useState(false); //modal    
 
   useEffect(() => {
@@ -37,25 +38,28 @@ const HomePage = () => {
   const onFileUpload = async () => {
     setResultado(null)
     setResultado2(null)
-    setLoading(true)
+    setLoadingWhisper(true)
     const formData = new FormData();
     formData.append('file', file);
     formData.append('model', 'whisper-1');
 
     try {
-      const response = await axios.post('https://api.openai.com/v1/audio/transcriptions', formData, {
-        headers: {
+      const response = await axios.post('https://api.openai.com/v1/audio/transcriptions',
+        formData,
+      { headers:
+         {
           'Content-Type': 'multipart/form-data',
           'Authorization': `Bearer ${import.meta.env.VITE_API_KEY}`
-        }
-      });
+         }
+      }
+    );
 
       /* console.log(response.data); */
       setResultado(response.data.text);
     } catch (error) {
       console.error(error);
     }
-    setLoading(false)
+    setLoadingWhisper(false)
   };
 
   const openai = new OpenAI({
@@ -64,23 +68,26 @@ const HomePage = () => {
 
   useEffect(() => {
     async function main() {
-      setLoading(true)
+      setLoadingGpt(true)
       if (resultado !== null) {
         const completion = await openai.chat.completions.create({
           messages: [{
             role: 'user', content: `
-            Usa la sintaxis y estructura profesional de markdown respetando titulo subtitulos, Texto en negrita, listas desordenadas etc. para la siguiente transcripcion de una reunion de Meet: " ${resultado} ".(sigue este ejemplo de la estructura respetando los 6 puntos):
+            Usa la sintaxis y estructura profesional de markdown respetando titulo subtitulos, Texto en negrita, listas desordenadas etc. para la siguiente transcripcion de una reunion de Meet: " ${resultado} ".(sigue este ejemplo de la estructura respetando los 7 puntos):
               1. titulo de la reunion
   
-              2. **Objetivo de la reunión**: Esboza el propósito o los objetivos de la reunión.
+              2. **Objetivo de la reunión**: Si están mencionados en la transcripción, resúmelos. Si no, escribe un breve resumen basado en el contexto.
               
               3. **Agenda**: Enumera los temas que se discutieron en la reunión, solamente si es que existe (no inventar).
               
               4. **Detalles de la discusión**: Este es el cuerpo principal del resumen. Aquí se incluirán los puntos de discusión de cada tema en la agenda, las ideas compartidas, y las decisiones tomadas. Debe estar estructurado de manera que cada tema discutido tenga su propio subtítulo y debajo de este, los puntos discutidos, solamente si es que existe (no inventar).
               
-              5. **Acciones a seguir**: Enumera todas las tareas o acciones que se acordaron durante la reunión, incluyendo quién es responsable de cada tarea y cualquier fecha límite relevante, solamente si es que existe (no inventar).
+              5. **Acciones a seguir**:Si están mencionados en la transcripción, Enumera todas las tareas o acciones que se acordaron durante la reunión, si no esta mensionado debes incluir una sugerencia o propuesta de acciones a seguir (incluye un texto que diga que es una propuesta generada por IA).
+              Debe incluir quién es responsable de cada tarea y cualquier fecha límite relevante,si no se nombran en la transcripcion ni un responsable ni fecha limite en el meet, se debe dejar en blanco.
+
+              6. **Asuntos Pendientes**: Temas que no se trataron y que se posponen para una próxima reunión, solamente si es que existen.
               
-              6. **Conclusiones**: Aquí debes resumir brevemente las conclusiones de la reunión. ¿Se lograron los objetivos de la reunión? ¿Qué se espera que ocurra como resultado de la reunión?)`
+              7. **Conclusiones**: Aquí debes resumir brevemente las conclusiones de la reunión. ¿Se lograron los objetivos de la reunión? ¿Qué se espera que ocurra como resultado de la reunión?)`
           }],
           model: "gpt-3.5-turbo-1106",
           max_tokens: 3500,
@@ -90,7 +97,7 @@ const HomePage = () => {
         setResultado2(completion.choices[0].message.content)
 
       }
-      setLoading(false)
+      setLoadingGpt(false)
     }
 
     main()
@@ -138,7 +145,7 @@ const HomePage = () => {
       <section className={style.container__form}>
         <article {...getRootProps()} className={`${style.upFile} ${isDragActive || file ? style.upFile_active : null}`}>
           <input {...getInputProps()} />
-          {loading ? <div className={style.container__loading}><Loader /> espere un momento...</div> :
+          {loadingWhisper || loadingGpt ? <div className={style.container__loading}><Loader /> {loadingWhisper ? "Transcribiendo..." : "Generando minuta..."}</div> :
             isDragActive
               ? <p className={style.container__textUP}>Suelta el archivo de audio aquí... </p>
               : !file
